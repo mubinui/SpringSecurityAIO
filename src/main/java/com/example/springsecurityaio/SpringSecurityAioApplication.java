a93e5048b170be93c787f6868e98ae8c5f3e0c76
uicmu <mubin41@student.sust.edu>
2023-01-07 22:58:21 +0600

    Initial commit Need fixes

---
+++ src/main/java/com/example/springsecurityaio/SpringSecurityAioApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurityaio/SpringSecurityAioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
